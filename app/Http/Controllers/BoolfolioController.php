e310020eee8f2eb6f7276050c28054d44a1fd781
francesco madonna <160396542+francescomadonna712@users.noreply.github.com>
2024-07-09 19:30:13 +0200

    creazione progetto e viste index create

---
+++ app/Http/Controllers/BoolfolioController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\boolfolio;
+use Illuminate\Http\Request;
+
+class BoolfolioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $boolfolios = Boolfolio::all();
+        return view('boolfolios.index', compact('boolfolios'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('boolfolios.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(boolfolio $boolfolio)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(boolfolio $boolfolio)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, boolfolio $boolfolio)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(boolfolio $boolfolio)
+    {
+        //
+    }
+}
