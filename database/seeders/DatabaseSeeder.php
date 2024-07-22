a969e147fe64013664d59f0b4340fd5e68e99cc6
francesco madonna <160396542+francescomadonna712@users.noreply.github.com>
2024-07-22 15:43:30 +0200

    modifiche a db seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             BoolfolioSeeder::class,
             UserSeeder::class,
-            CategorySeeder::class,
+
         ]);
     }
 }
